461a3479855fa67fedc7e0147544091b2f73e16b
FrAndres7777 <fanaconaperafan@gmail.com>
2023-04-10 08:05:08 -0500

    me rindo

--- CAR/m2.h
+++ CAR/m2.h
@@ -2,23 +2,10 @@
 #include <fstream>
 #include <bitset>
 #include <string>
-
+#include "FUNCIONESMETODO1FINAL.h"
 using namespace std;
 // funcion regla
-int Regla(int ceross, int unoss) {
-    if (ceross == unoss) {
-        return 1;
-    } else if (ceross > unoss) {
-        return 2;
-    } else {
-        return 3;
-    }
-}
 
-// Función para convertir un caracter a su representación binaria
-string charToBinary(char c) {
-    return bitset<8>(c).to_string();
-}
 
 
 // Función para convertir una cadena de caracteres a su representación binaria
@@ -100,72 +87,24 @@ void codix(string name,int semi){
 
 
 void codix1(string name,int semi){
-	char str[256];
+	 char str[256];
     ifstream fin;
     fin.open(name);
-    ofstream fout;
-    fout.open("archiv.txt");
     if (fin.is_open()) cout<<"Abierto"<<endl;
     else cout<<"No abierto"<<endl;
+
     //Para LEEER POR LINEA
     cout<<"Por Linea: "<<endl;
     string hi;
-    int ceros=0,regla=1;
-	int cont=0,unos=0;
-	int bloque=1,i2 = 0,i3=1;
-	char list[semi];
     while(fin.good()){
-    	bloque++;
-        char temp= fin.get();
-        list[bloque-1]= temp;
-        cont++;
-        if(temp=='0'){
-        	ceros++;
-		}else if (temp=='1'){
-			unos++;
+        fin.getline(str, 256);
+        for(int i = 0 ; str[i]!='\n';i++){
+        	hi+=str[i];
 		}
-		//
-		if(regla==1){
-			if(temp='1'){
-				fout<<0;
-			}else{
-				fout<<1;
-			}
-		}else if(regla ==2){
-			if (i2 % 2 == 1 or i2 ==1 ) {
-		           	if(temp== '0'){
-		        		fout<<1;
-					}else if (temp== '1'){
-		        		fout<<0;
-						}
-		        	}else{
-		        		fout<<temp;
-					}
-   		
-			}else{
-				if(i3 %3==0){
-	            	if(temp== '0'){
-	        			fout<<1;
-					}else if (temp== '1'){
-	        			fout<<0; 
-					}
-   				}else{
-   					fout<<temp;
-				   }
-			}
+		hi=codilinea(hi);
 		
-		
-		
-		//
-		if(bloque==semi){
-			bloque=0;
-			regla=Regla(ceros,unos);
-			ceros=0;unos=0;
-			
-		
-		}i2++,i3++;
-		    
-    }
-	fin.close();
-	fout.close();
+    }ofstream fout;
+    fout.open("archiv.txt");
+    fout<<hi;
+    fout.close();
 }
